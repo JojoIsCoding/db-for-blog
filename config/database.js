0a631e8fa2452f023efdd228a8ee0f527a73aaa6
JojoIsCoding <98815845+JojoIsCoding@users.noreply.github.com>
2022-06-16 15:55:04 +0300

    update #1354321321

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
-module.exports = ({
+/* module.exports = ({
   env
 }) => ({
   defaultConnection: 'default',
@@ -25,3 +25,19 @@ module.exports = ({
   },
 });
 
+ */
+
+module.exports = ({ env }) => ({
+    defaultConnection: "default",
+    connection: {
+      client: "postgres",
+      connection: {
+        host: env("DATABASE_HOST", "localhost"),
+        port: env.int("DATABASE_PORT", 5432),
+        database: env("DATABASE_NAME", "bank"),
+        username: env("DATABASE_USERNAME", "postgres"),
+        password: env("DATABASE_PASSWORD", "0000"),
+        schema: env("DATABASE_SCHEMA", "public"),
+      },
+    }
+  });
--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,4 +1,4 @@
-module.exports = ({
+/* module.exports = ({
   env
 }) => ({
   defaultConnection: 'default',
@@ -22,5 +22,24 @@ module.exports = ({
               ssl: env.bool('DATABASE_SSL', false),
           },
       },
+  },
+}); */
+
+const parse = require('pg-connection-string').parse;
+const config = parse(process.env.DATABASE_URL);
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'postgres',
+    connection: {
+      host: config.host,
+      port: config.port,
+      database: config.database,
+      user: config.user,
+      password: config.password,
+      ssl: {
+        rejectUnauthorized: false
+      },
+    },
+    debug: false,
   },
 });
