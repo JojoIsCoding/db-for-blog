66493d701777e61221a6df3314c74438076b36c4
JojoIsCoding <98815845+JojoIsCoding@users.noreply.github.com>
2022-06-16 13:47:11 +0300

    direct value of port

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
     proxy: true,
     url: env('MY_HEROKU_URL'),
-    port: env.int('PORT', process.env.PORT || 1337),
+    port: process.env.PORT || 1337,
     app: { 
       keys: env.array('APP_KEYS')
     },
